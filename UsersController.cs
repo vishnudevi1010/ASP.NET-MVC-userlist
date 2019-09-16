6891603e44234d99bc46aa83e1c8e649b5dbe234
vishnudevi1010 <55373334+vishnudevi1010@users.noreply.github.com>
2019-09-16 10:37:25 +0530

    Add files via upload

---
+++ UsersController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using entityframe.Models.entities;
+using Microsoft.AspNetCore.Http;
+
+namespace entityframe.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly sqlqueryContext _context;
+        private sqlqueryContext db = new sqlqueryContext();
+       // DbContext db = new sqlqueryContext();
+        public UsersController(sqlqueryContext context)
+        {
+            _context = context;
+           // db = context;
+        }
+
+        // GET: Users
+        [HttpGet]
+
+       /* public async Task<IActionResult> Index()
+        {
+            return View(await _context.User.ToListAsync());
+        }*/
+        public ActionResult Index()
+        {
+          
+           
+            return View(db.User.ToList());
+        }
+       [HttpPost]
+        public ActionResult Index(string searchstring)
+        {
+            // string searchstring = id;
+              var users = from u in db.User
+                         select u;
+              if (!String.IsNullOrEmpty(searchstring))
+              {
+                  users = users.Where(s => s.FirstName.Contains(searchstring));
+              }
+
+          //  var users=db.User.ToList().Where(emp => emp.FirstName == searchstring || searchstring == null).ToList();
+           // return RedirectToAction(nameof(Index));
+            return View(users);
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+          return  View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FirstName,LastName,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.CreationDate = DateTime.UtcNow;
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,CreationDate")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.Id == id);
+        }
+    }
+}
