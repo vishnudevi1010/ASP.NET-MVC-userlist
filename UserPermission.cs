9dabacaebc144f3ec1918aa94afa0c1f9ce5db0b
vishnudevi1010 <55373334+vishnudevi1010@users.noreply.github.com>
2019-09-16 10:41:16 +0530

    Add files via upload

---
+++ UserPermission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace entityframe.Models.entities
+{
+    public partial class UserPermission
+    {
+        public int UserId { get; set; }
+        public int PermissionId { get; set; }
+
+        public virtual Permission Permission { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ sqlqueryContext.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace entityframe.Models.entities
+{
+    public partial class sqlqueryContext : DbContext
+    {
+        internal object user;
+
+        public sqlqueryContext()
+        {
+        }
+
+        public sqlqueryContext(DbContextOptions<sqlqueryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Permission> Permission { get; set; }
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<UserCredential> UserCredential { get; set; }
+        public virtual DbSet<UserPermission> UserPermission { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Permission>(entity =>
+            {
+                entity.Property(e => e.Code)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<UserCredential>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.HashedPassword)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.PasswordSalt)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.IdNavigation)
+                    .WithOne(p => p.UserCredential)
+                    .HasForeignKey<UserCredential>(d => d.Id)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_UserCredential_User");
+            });
+
+            modelBuilder.Entity<UserPermission>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.PermissionId })
+                    .HasName("PK_UserPermissions");
+
+                entity.HasOne(d => d.Permission)
+                    .WithMany(p => p.UserPermission)
+                    .HasForeignKey(d => d.PermissionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_UserPermissions_Permissions");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.UserPermission)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_UserPermissions_Users");
+            });
+        }
+    }
+}
